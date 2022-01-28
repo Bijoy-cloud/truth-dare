ca34ffe3e3182460e030c6792ff1e4ea73d1a10a
[Bijoy-cloud] <[bishalbijoy98@gmail.com]>
2022-01-28 13:45:30 +0530

    random slice color

--- src/component/Board/board.js
+++ src/component/Board/board.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./board.css";
 
 function Board(props) {
-  // var degree =  (i * share)deg skeyY = (share - 90)deg i++;
+ 
   const values = [
     {
       color: "#ced206",
@@ -44,20 +44,19 @@ function Board(props) {
       color: "#ca5adc",
     },
   ];
-  // Math.floor(Math.random()*16777215).toString(16)
+ 
   function showBoard(user) {
     if (user > 2) {
       const rotateAngle = 360 / user;
       var skewAngle = rotateAngle - 90;
 
       const arr = values.slice(0, user);
-      // console.log(arr)
       return arr.map((item, index) => {
         return (
           <li key={index}
             style={{
               transform: `rotate(${index * rotateAngle}deg) skewY(${skewAngle}deg)`,
-              background:  `${item.color}`,
+              background:  `#${Math.floor(Math.random()*16777215).toString(16)}`,
             }}
           >
             {/* {console.log('inside map',index*rotateAngle)} */}
