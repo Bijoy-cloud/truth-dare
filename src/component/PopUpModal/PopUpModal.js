9f8b31222cfea2e29219a84a32a89879842b73cd
Bijoy Saha <bishalbijoy98@gmail.com>
2022-01-31 15:23:50 +0530

    production ready

--- src/component/PopUpModal/PopUpModal.js
+++ src/component/PopUpModal/PopUpModal.js
@@ -1,27 +1,36 @@
 import React from "react";
 import "./popUpModal.scoped.css";
 import logo from "./menu-logo.svg";
-// import { Data,General } from "../data";
-
+import { Button } from "@mui/material";
+import { AiOutlineClose } from 'react-icons/ai';
 function PopUpModal(props) {
-  
+ 
   if (!props.show) return null;
   else
- 
-
     return (
       <div className="mymodal">
         <div className="modal">
           <div className="modal-header">
             <img className="logo" src={logo} alt="icon" />
-          </div>
-          <div className="modal-content">
-          {props.modalContent}
-          </div>
+            <div className="close-icon">
+            <AiOutlineClose/> 
+            </div>         </div>
+          <div className="modal-content">{props.modalContent}</div>
           <div className="modal-footer">
-            <button className="close" onClick={props.onHide}>
+            <Button
+              style={{
+                backgroundColor: "grey",
+              }}
+              size="small"
+              variant="contained"
+              onClick={props.onHide}
+            >
+              Close
+            </Button>
+
+            {/* <button className="close" onClick={props.onHide}>
               close
-            </button>
+            </button> */}
           </div>
         </div>
       </div>
