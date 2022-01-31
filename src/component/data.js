6077ce83c5cf811973bcf4066d6ae1cf28a63700
Bijoy Saha <bishalbijoy98@gmail.com>
2022-01-31 23:32:33 +0530

    production ready

--- src/component/data.js
+++ src/component/data.js
@@ -1,27 +1,34 @@
 export const data18 = [
-   
-    "Have you ever sexted anyone? If so, read one of the sexts in a fake British accent.",
+    "Do you like loud or silent people in bed?",
+    "What’s your biggest turn off?",
+    "What is your favourite song when it comes to action in the bedroom?",
+    "What your maddest one-night stand story?",
+    "Have you ever s*xted anyone? If so, read one of the sexts in a fake British accent.",
     "What’s your favorite fantasy to pleasure yourself to?",
     "What’s your favorite body part on yourself? On your partner?",
     "Where is the naughtiest place you have done it?",
     "Is there anything that turns you off in the bedroom?",
     "Have you ever had a friend with benefits?",
-    "What is your favorite sex position?",
+    // "What is your favorite s*x position?",
     "What’s the sexiest thing someone could say to you in bed?",
-    "Where’s the weirdest place you’ve had sex?",
-    "Name a sex position you’d like to try",
-    "Would you/have you had group sex?",
-    "When it comes to BDSM, how far have you gone/would go?",
+    // "Where’s the weirdest place you’ve had sex?",
+    "Name a s*x position you’d like to try",
+    // "Would you/have you had group sex?",
+    // "When it comes to BDSM, how far have you gone/would go?",
     "Where’s the weirdest place you’ve ever masturbated?",
     "Do you like to be spanked?",
-    "What was your most embarrassing sexual experience?"
+    // "What was your most embarrassing sexual experience?"
 ]
 
 export const data = [
+    "What have you bought that’s been the biggest waste of money?",
+    " If you could become invisible, what’s the worst thing you’d do?",
+    "Who is the most surprising person to ever slide into your DMs?",
+    "Who in the room would be the worst person to date and why?",
     "Who is ur childhood crush?",
     "Have you ever cheated on exam?",
     "Who is ur celebrity crush?",
-    "what isthe most naughtiest thing you have done in your school/college?",
+    "what is the most naughtiest thing you have done in your school/college?",
     "Ex-girlfriend or Crush , whom do you wanna meet?",
     "your partner or bestfriend whom do you wanna kill?"
 
